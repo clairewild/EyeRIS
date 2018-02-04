e69784bf953902affa57ac1781dab362d03b7ae8
Akansh Murthy <ak45m16@gmail.com>
2018-02-04 00:04:20 -0800

    Make graphs work

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
     await client.start();
     
     client.eegReadings.subscribe(reading => {
-      console.log(reading);
+      this.plot(reading)
     });
     
     this.setState({
@@ -53,6 +53,42 @@ class App extends React.Component {
     document.body.removeChild(a);
   }
   
+  async plot(reading: EEGReading) {
+    const channelNames = [
+      'TP9',
+      'AF7',
+      'AF8',
+      'TP10',
+      'AUX',
+    ];
+    const graphTitles = Array.from(document.querySelectorAll('.electrode-item h3'));
+    const canvases = Array.from(document.querySelectorAll('.electrode-item canvas'));
+    const canvasCtx = canvases.map((canvas) => canvas.getContext('2d'));
+  
+    graphTitles.forEach((item, index) => {
+        item.textContent = channelNames[index];
+    });
+    
+    const canvas = canvases[reading.electrode];
+    const context = canvasCtx[reading.electrode];
+    if (!context) {
+        return;
+    }
+    const width = canvas.width / 12.0;
+    const height = canvas.height / 2.0;
+    context.fillStyle = 'green';
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < reading.samples.length; i++) {
+        const sample = reading.samples[i] / 15.;
+        if (sample > 0) {
+            context.fillRect(i * 25, height - sample, width, sample);
+        } else {
+            context.fillRect(i * 25, height, width, -sample);
+        }
+    }
+  }
+  
   render() {
     const message = this.state.connected ? "We're listening to your brain activity!" : "Click the button to connect.";
     
@@ -65,6 +101,31 @@ class App extends React.Component {
         <p className="App-intro">{ message }</p>
         
         <button onClick={ this.museConnect }>Start brain recording</button>
+        
+        <div className="electrode-set">
+          <div className="electrode-item">
+  	        <h3>Electrode 1</h3>
+  	        <canvas id="electrode1"></canvas>
+          </div>
+          
+          <div className="electrode-item">
+  	        <h3>Electrode 2</h3>
+  	        <canvas id="electrode2"></canvas>
+          </div>
+        </div>
+        
+        <div className="electrode-set">
+          <div className="electrode-item">
+  	        <h3>Electrode 3</h3>
+  	        <canvas id="electrode3"></canvas>
+          </div>
+          
+          <div className="electrode-item">
+  	        <h3>Electrode 4</h3>
+  	        <canvas id="electrode4"></canvas>
+          </div>
+        </div>
+        
         <br/>
         <br/>
         <br/>
@@ -76,6 +137,8 @@ class App extends React.Component {
           strokeColor={"red"}    
           backgroundColor={"black"}
         />
+        <br/>
+        <br/>
         <button onClick={ this.startAudioRecording }>Start audio recording</button>
         <button onClick={ this.stopAudioRecording }>Stop audio recording</button>
 
