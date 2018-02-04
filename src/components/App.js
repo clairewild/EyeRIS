69fbef5c1d5c2178aa604e49e21ed3bec24a8d49
Claire Wild <claire@zesty.com>
2018-02-03 17:52:33 -0800

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { MuseClient } from 'muse-js';
+
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      connected: false
+    };
+    this.connect = this.connect.bind(this);
+  }
+  
+  async connect() {
+    let client = new MuseClient();
+    await client.connect();
+    await client.start();
+    
+    client.eegReadings.subscribe(reading => {
+      console.log(reading);
+    });
+    
+    this.setState({
+      connected: true
+    });
+  }
+  
+  render() {
+    const message = this.state.connected ? "You are connected!" : "Click the button to connect.";
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Neurofeedback Experimenting...</h1>
+        </header>
+        
+        <p className="App-intro">{ message }</p>
+        
+        <button onClick={ this.connect }>CONNECT MUSE</button>
+      </div>
+    );
+  }
+}
+
+export default App;
