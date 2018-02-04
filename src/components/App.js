fb4f5f96cd7798ab793f1b3bbe4883eb6aabe8cb
Claire Wild <claire@zesty.com>
2018-02-03 22:53:17 -0800

    Create csv for muse data

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,13 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      connected: false
+      connected: false,
+      recordingMuse: false,
+      client: null
     };
     this.connect = this.connect.bind(this);
+    this.recordMuse = this.recordMuse.bind(this);
+    this.saveCSV = this.saveCSV.bind(this);
   }
   
   async connect() {
@@ -17,17 +21,43 @@ class App extends React.Component {
     await client.connect();
     await client.start();
     
-    client.eegReadings.subscribe(reading => {
-      console.log(reading);
-    });
-    
     this.setState({
-      connected: true
+      connected: true,
+      client: client
     });
   }
   
+  async recordMuse() {
+    this.setState({ recordingMuse: true });
+    
+    let samples = [];
+    let client = this.state.client;
+    await client.eegReadings.subscribe(reading => {
+      reading.samples.forEach(s => {
+        samples.push([reading.timestamp, reading.electrode, s]);
+      });  
+    });
+    await window.setInterval(() => this.saveCSV(samples), 5000);
+  }
+  
+  async saveCSV(samples) {
+    this.setState({ recordingMuse: false });
+    
+    const a = document.createElement('a');
+    const headers = ["time", "electrode", "microvolts"].join(",");
+    const csvData = headers + "\n" + samples.map(item => item.join(",")).join("\n");
+    const file = new Blob([csvData], { type: "text/csv" });
+    a.href = URL.createObjectURL(file);
+    document.body.appendChild(a);
+    a.download = "museRecording.csv";
+    a.click();
+    document.body.removeChild(a);
+  }
+  
   render() {
-    const message = this.state.connected ? "We're listening to your brain activity!" : "Click the button to connect.";
+    const button = (this.state.connected) ? 
+      <button onClick={ this.recordMuse }>START RECORDING</button> :
+      <button onClick={ this.connect }>CONNECT MUSE</button>;
     
     return (
       <div className="App">
@@ -35,9 +65,7 @@ class App extends React.Component {
           <h1 className="App-title">Eyeris</h1>
         </header>
         
-        <p className="App-intro">{ message }</p>
-        
-        <button onClick={ this.connect }>CONNECT MUSE</button>
+        { (this.state.recordingMuse) ? <p>We are recording your brain activity</p> : button }
       </div>
     );
   }
